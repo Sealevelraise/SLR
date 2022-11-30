716de1b6f70880ebcf8de8ff11c085ce21138289
Josua Benz <josua.benz@student.htw-berlin.de>
2022-11-30 17:51:45 +0100

    truffle init

---
+++ contracts/Donate.sol
@@ -0,0 +1,14 @@
+// SPDX-License-Identifier: MIT
+pragma solidity >=0.4.22 <0.9.0;
+
+contract Donate {
+    //mapping if user has donated money
+    mapping(address => uint) public userHasDonated;
+
+    function donateEther(uint amount) public {
+        //TODO hier muss der Bazahlvorgang noch erfolgen
+
+        //mapping that this user has donated money to SLR
+        userHasDonated[msg.sender] = 1;
+    }
+}
