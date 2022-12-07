1c49252c422850929f2b68f2f3211e4c3927b960
franziska.schicks <franziska.schicks@ensolarado.de>
2022-12-07 13:56:21 +0100

    beispielprojekt angelegt, design erstmal nur auf desktop optimiert

--- nuxt/components/FooterMain.vue
+++ nuxt/components/FooterMain.vue
@@ -1,14 +1,11 @@
 <template>
-    <div class=" bg-slr-blue h-30">
-
-    </div>
+  <div class="bg-slr-blue h-30">
+    <ConnectWallet class="mt-10 p-2 py-8" />
+  </div>
 </template>
 
-
 <script>
-    export default{
-        name: 'FooterMain',
-    }
-
-
+export default {
+  name: 'FooterMain',
+}
 </script>
--- nuxt/tailwind.config.js
+++ nuxt/tailwind.config.js
@@ -1,31 +1,30 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: [
-    "./components/**/*.{js,vue,ts}",
-    "./layouts/**/*.vue",
-    "./pages/**/*.vue",
-    "./plugins/**/*.{js,ts}",
-    "./nuxt.config.{js,ts}",
+    './components/**/*.{js,vue,ts}',
+    './layouts/**/*.vue',
+    './pages/**/*.vue',
+    './plugins/**/*.{js,ts}',
+    './nuxt.config.{js,ts}',
   ],
   //
   theme: {
-    extend: {    
+    extend: {
       colors: {
-      'slr-blue': '#0051A0',
-      'slr-page-bg': '#F5F5F5',
+        'slr-blue': '#0051A0',
+        'slr-page-bg': '#FDF5E0',
       },
       fontFamily: {
-        'robotoSlab':['RobotoSlab'],
-        'roboto':['Roboto'],
+        robotoSlab: ['RobotoSlab'],
+        roboto: ['Roboto'],
       },
     },
-  container: {},
+    container: {},
   },
   //
   variants: {
-  objectFit: ['responsive', 'hover', 'focus'],
+    objectFit: ['responsive', 'hover', 'focus'],
   },
   //
   plugins: [],
-
 }
