3789e179a59e0471cbc6c1ff03bb1bb7bdc20abe
Josua Benz <josua.benz@student.htw-berlin.de>
2022-12-09 17:40:19 +0100

    get User Account from MetaMask instead of hard-coded use input-fields for adding a new project with AddProject Smart Contract
    Co-authored-by: Laura Schönherr <laura-schoenherr@users.noreply.github.com>

--- vue-web3js-project/src/App.vue
+++ vue-web3js-project/src/App.vue
@@ -3,60 +3,70 @@
     <!-- connect-wallet button is visible if the wallet is not connected -->
     <button v-if="!connected" @click="connect">Connect wallet</button>
     <!-- call-contract button is visible if the wallet is connected -->
-    <button v-if="connected" @click="callContract">Call contract</button>
-
-    <button @click="createNewProject">AddNewProject</button>
+    <button v-if="connected" @click="callContract">get number of current projects</button>
+    <p v-if="connected">project name:</p><input type="text" v-model="htmlProjectName" placeholder="type in project name here" v-if="connected">
+    <p v-if="connected">country:</p><input type="text" v-model="htmlCountry" placeholder="type in country here" v-if="connected">
+    <p v-if="connected">amount:</p><input type="text" v-model="htmlAmount" placeholder="type in amount here" v-if="connected">
+    <button v-if="connected" @click="createNewProject">add new project</button>
     {{ contractResult }}
   </div>
 </template>
 
 <script>
 import Web3 from 'web3'
-//import DonateJsonFile from './Donate.json'
+//import DonateJson from './Donate.json'
 import AddProjectJson from './AddProject.json'
-//import VoteJsonFile from './Vote.json'
+//import VoteJson from './Vote.json'
 
 export default {
   name: 'App',
 
   data() {
     return {
+      htmlProjectName: '',
+      htmlCountry: '',
+      htmlAmount: '',
+      AddProjectAddr: '0x9d01fE4313C9ef0D0B3BAb5fCf14d6548f5E4a18',
+      DonateAddr: '0x5a2b07D8cBF5687B2cA8E09429771e992211A6e2',
+      VoteAddr: '0xCF722672e49a37149bD57575b2E674c4720bfF82',
       connected: false,
+      connectedAccounts: '', //account-IDs will be safed in this variable
       contractResult: '',
     }
   },
 
   methods: {
-    connect: function () {
+    connect: async function () {
       // this connects to the wallet
       if (window.ethereum) { // first we check if metamask is installed
-        window.ethereum.request({method: 'eth_requestAccounts'})
-            .then(() => {
-              this.connected = true; // If users successfully connected their wallet
-              //TODO erfolgreich verbunden ausgeben
+        try {
+            this.connectedAccounts = await window.ethereum.request({
+                method: "eth_requestAccounts",
             });
+            this.connected = true
+            console.log(this.connectedAccounts.toString())
+        } catch (error) {
+            console.log(error)
+
+        }
       }
     },
     callContract: function () {
       // method for calling the contract method
       let web3 = new Web3(window.ethereum);
-      let contractAddress = '0x9d01fE4313C9ef0D0B3BAb5fCf14d6548f5E4a18';
 
-      //let abi = JSON.parse(AddProjectJsonFile);
-      let contract = new web3.eth.Contract(AddProjectJson.abi, contractAddress);
-
+      let contract = new web3.eth.Contract(AddProjectJson.abi, this.AddProjectAddr);
+      
       contract.methods.getNumberOfProjects().call()
           .then(result => this.contractResult = result);
     },
     createNewProject: function () {
       // method for calling the contract method
       let web3 = new Web3(window.ethereum);
-      let contractAddress = '0x9d01fE4313C9ef0D0B3BAb5fCf14d6548f5E4a18cd';
       
-      //let abi = JSON.parse(AddProjectJsonFile);
-      let contract = new web3.eth.Contract(AddProjectJson.abi, contractAddress);
+      let contract = new web3.eth.Contract(AddProjectJson.abi, this.AddProjectAddr);
 
-      contract.methods.addProject('HilfeFuerCharlie', 'Berlin', 10).send({from: '0x3feed9809DffbbAc15adF4C57bD8A12cc0599aaB', gas: 6721975})
+      contract.methods.addProject(this.htmlProjectName, this.htmlCountry, parseInt(this.htmlAmount)).send({from: this.connectedAccounts[0], gas: 6721975})
           .then(result => this.contractResult = result);
     }
   },
