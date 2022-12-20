e1f73bce97fd043dfe03d19233dfa7dfbb13ebba
Benjamin Loncarevic <benjamin.loncarevic@student.htw-berlin.de>
2022-12-20 11:53:19 +0100

    Eingabefeld und Button

--- nuxt/pages/spenden.vue
+++ nuxt/pages/spenden.vue
@@ -1,7 +1,15 @@
 <template>
   <div>
     <h1>Spenden</h1>
+    <p>Betrag eingeben</p>
+    <input v-model="message" placeholder="Spendenbetrag">
+
+    <button>
+      <p>Absenden</p>
+    </button>
+
   </div>
+
 </template>
 
 <script>
