86482bf212a4ce973f48ce9ecf30d60f72797d85
Nicolas von der Ahe <nicolas.vonderahe@fruuts.com>
2022-12-21 09:40:17 +0100

    first tests for donate contract at library truffle-assertions for revert() test

--- truffle-project/test/donate.js
+++ truffle-project/test/donate.js
@@ -1,4 +1,5 @@
 const Donate = artifacts.require("./Donate.sol");
+const truffleAssert = require('truffle-assertions');
 
 contract("Donate", (accounts) => {
     it("test balance of the contract after donation", async () => {
@@ -9,12 +10,21 @@ contract("Donate", (accounts) => {
         // Get the balance before the operation
         let balanceBefore = await contract.getContractBalance();
         console.log(parseInt(balanceBefore));
-        // Send 3 gwei
-        await contract.donateEther({ from: account, value: 3});
-        // Get the balance after the gwei was sent
+        // Send 1 finney
+        await contract.donateEther({ from: account, value: 1000000000000000 });
+        // Get the balance after the finney was sent
         let balanceAfter = await contract.getContractBalance();
         console.log(parseInt(balanceAfter));
-        // Check if the balance after is equal to the balance before plus 3 gwei
-        assert.equal(balanceAfter.toString(), parseInt(balanceBefore.toString()) + 3);
+        // Check if the balance after is equal to the balance before plus 1 finney
+        assert.equal(balanceAfter.toString(), parseInt(balanceBefore.toString()) + 1000000000000000);
     });
+    it("should not be able to donate less then 1 finney", async () => {
+        // Get the first account
+        let account = accounts[0];
+        // Retreive the deployed contract
+        const contract = await Donate.deployed();
+        await truffleAssert.reverts(contract.donateEther({ from: account, value: 1 }));
+        
+    });
+
 });
