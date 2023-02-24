8178079a60626908735b996dc0eb7f43e66ed534
franziska.schicks <franziska.schicks@ensolarado.de>
2023-02-24 12:02:55 +0100

    account icon und default account seite angelegt

--- nuxt/components/ConnectWallet.vue
+++ nuxt/components/ConnectWallet.vue
@@ -1,9 +1,21 @@
 <template>
   <div id="app">
     <!-- connect-wallet button is visible if the wallet is not connected -->
-    <button v-if="!connected" @click="connect">Connect wallet</button>
+    <button
+      class="w-11/12 md:w-2/5 text-slr-blue border-solid border-2 border-slr-blue bg-slr-page-bg duration-500 py-2 px-6 rounded-md"
+      v-if="!connected"
+      @click="connect"
+    >
+      Connect wallet
+    </button>
     <!-- call-contract button is visible if the wallet is connected -->
-    <button v-if="connected" @click="createNewProject">Add new Project</button>
+    <button
+      class="w-11/12 md:w-2/5 text-slr-blue border-solid border-2 border-slr-blue bg-slr-page-bg duration-500 py-2 px-6 rounded-md"
+      v-if="connected"
+      @click="createNewProject"
+    >
+      Add new Project
+    </button>
     {{ contractResult }}
   </div>
 </template>
@@ -13,7 +25,7 @@ import Web3 from 'web3'
 import AddProjectJson from '../../truffle-project/build/contracts/AddProject.json'
 
 export default {
-  name: "ConnectWallet",
+  name: 'ConnectWallet',
 
   data() {
     return {
@@ -30,33 +42,39 @@ export default {
   methods: {
     connect: async function () {
       // this connects to the wallet
-      if (window.ethereum) { // first we check if metamask is installed
+      if (window.ethereum) {
+        // first we check if metamask is installed
         try {
-            // we save the account id(s) to 'connectedAccounts'
-            this.connectedAccounts = await window.ethereum.request({
-                method: "eth_requestAccounts",
-            });
-            this.connected = true
-            console.log(this.connectedAccounts.toString())
+          // we save the account id(s) to 'connectedAccounts'
+          this.connectedAccounts = await window.ethereum.request({
+            method: 'eth_requestAccounts',
+          })
+          this.connected = true
+          console.log(this.connectedAccounts.toString())
         } catch (error) {
-            console.log(error)
-
+          console.log(error)
         }
       }
     },
     createNewProject: async function (projectName, country, amount) {
       // method for adding a new project to the blockchain
-      const web3 = new Web3(window.ethereum);
-      
-      const contract = new web3.eth.Contract(AddProjectJson.abi, this.AddProjectAddr);
-      // the parameters of the contract will be filled with the text in the input-fields
-      this.contractResult = await contract.methods.addProject(this.addProject_Name, this.addProject_Country, parseInt(this.addProject_Amount)).send({from: this.connectedAccounts[0], gas: 6721975});
-    },
-  }
+      const web3 = new Web3(window.ethereum)
 
+      const contract = new web3.eth.Contract(
+        AddProjectJson.abi,
+        this.AddProjectAddr
+      )
+      // the parameters of the contract will be filled with the text in the input-fields
+      this.contractResult = await contract.methods
+        .addProject(
+          this.addProject_Name,
+          this.addProject_Country,
+          parseInt(this.addProject_Amount)
+        )
+        .send({ from: this.connectedAccounts[0], gas: 6721975 })
+    },
+  },
 }
 </script>
 
-<style scoped>
-
-</style>
+<style scoped></style>
