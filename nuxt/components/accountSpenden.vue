624444e868e3c1bd19bab29d6a1072dac0329e84
BenjaminLon <benjamin.loncarevic@gmail.com>
2023-02-24 12:06:04 +0100

    create account sites  "Spender" + "Projektowner"

--- nuxt/components/accountSpenden.vue
+++ nuxt/components/accountSpenden.vue
@@ -1,7 +1,7 @@
 <template>
   <div>
     <!--page to display project info, entered at "projektAnlegen"-->
-    <h1>Willkommen auf der Account Seite</h1>
+    <h1>Angemeldet als: Spender</h1>
     <div class="content-box bg-slr-blue-box">
       <div class="mx-auto p-10">
         <h2>Hier finden Sie die Angaben zu Ihrem Account</h2>
