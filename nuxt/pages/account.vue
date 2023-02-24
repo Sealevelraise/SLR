0405a4f813c8cd42fcab874a157c2df9e30cbe4b
franziska.schicks <franziska.schicks@ensolarado.de>
2023-02-24 13:54:23 +0100

    design changes

--- nuxt/pages/account.vue
+++ nuxt/pages/account.vue
@@ -5,8 +5,16 @@
     <div
       class="w-11/12 md:w-3/4 mx-auto bg-slr-blue-box py-10 md:py-12 px-6 md:px-32 rounded-[24px] text-white"
     >
-      <h2 class="text-2xl text-center pb-4 md:pb-8">Werde Teil von SLR!</h2>
-      <!-- p class="text-center pb-4">Ich möchte...</p>
+      <div v-if="spender">
+        <!-- TODO add function @laura -->
+        <accountSpenden />
+      </div>
+      <div v-else-if="projectowner">
+        <accountProjekt />
+      </div>
+      <div v-else>
+        <h2 class="text-2xl text-center pb-4 md:pb-8">Werde Teil von SLR!</h2>
+        <!-- p class="text-center pb-4">Ich möchte...</p>
 
       <div
         class="flex flex-col items-center md:flex-row md:place-content-between"
@@ -25,15 +33,16 @@
           Spenden erhalten
         </button>
       </div-->
-      <p class="text-center">
-        Mit Klick auf den Connect Wallet Button kannst Du Deine Wallet
-        verbinden. Mehr Infos dazu gibt es
-        <nuxt-link to="/spendenprozess"> hier </nuxt-link>. Nach erfolgreichem
-        verbinden kannst Du spenden und abstimmen oder deine Spenden und
-        Abstimmungen einsehen (als Spender) oder ein Projekt anlegen bzw. dein
-        angelegtes Projekt ansehen (als Projektinhaber).
-      </p>
-      <ConnectWallet class="mx-auto text-center mt-4" />
+        <p class="text-center">
+          Mit Klick auf den Connect Wallet Button kannst Du Deine Wallet
+          verbinden. Mehr Infos dazu gibt es
+          <nuxt-link to="/spendenprozess"> hier</nuxt-link>. Nach erfolgreichem
+          verbinden kannst Du spenden und abstimmen oder deine Spenden und
+          Abstimmungen einsehen (als Spender) oder ein Projekt anlegen bzw. dein
+          angelegtes Projekt ansehen (als Projektinhaber).
+        </p>
+        <ConnectWallet class="mx-auto text-center mt-4" />
+      </div>
     </div>
   </div>
 </template>
